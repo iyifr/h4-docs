c56b225b662ec3c793db993beec42eab8577d121
iyifr <adekileiyimide@gmail.com>
2024-10-01 10:18:32 +0100

    feat: fix bad mobile screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 
 export default function HomePage() {
   return (
-    <main className="flex h-full flex-col items-center text-center bg-cover bg-center bg-no-repeat bg-[url('https://assets.uploadfast.dev/Hypercolor%20Gradient%20(2)-27oj.jpeg')]">
+    <main className="flex lg:h-full h-screen flex-col items-center text-center bg-cover bg-center bg-no-repeat bg-[url('https://assets.uploadfast.dev/Hypercolor%20Gradient%20(2)-27oj.jpeg')]">
       <section className="flex items-center mt-[56px]">
         <span className="font-bold font-stylic text-6xl lg:text-[10rem] leading-none">H</span>
         <div className='lg:mx-8 mx-4 lg:text-5xl text-xl flex items-center gap-0'>
@@ -15,7 +15,7 @@ export default function HomePage() {
       </p>
       <a href="/docs"><button className="my-12 bg-orange-200 px-6 py-3 rounded-md font-code text-black/90">Read the documentation</button></a>
 
-      <section className="my-12">
+      <section className="my-12 lg:px-0 px-4">
         <img src="https://assets.uploadfast.dev/h4-example(2)-ar98.png" className="rounded-xl object-scale-down lg:w-[45em]" />
       </section>
     </main>
