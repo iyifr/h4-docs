cf40e84448a6d7bd65757f8e2eb2d020ac5cd5e2
iyifr <adekileiyimide@gmail.com>
2024-10-12 18:44:06 +0100

    seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,13 +6,20 @@ import type { ReactNode } from 'react';
 
 
 export const metadata: Metadata = {
-  title: 'H4 Dart.',
-  description: 'A delightful framework for building web servers with dart.',
+  title: 'H4.dart',
+  description: 'A delightful dart framework for the web.',
+  keywords: ['Dart', 'Server', 'Framework', 'Dart'],
   openGraph: {
     type: "website",
     url: "h4-tau.vercel.app",
     description: "A delightful framework for building web servers with dart.",
-    images: ""
+    siteName: "H4 Dart."
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'H4.dart',
+    description: 'A delightful dart framework for the web',
+    creator: '@iyifrr',
   }
 }
 
